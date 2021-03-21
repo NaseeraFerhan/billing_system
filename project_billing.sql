48610c198bdcbe5c4754fcf9f5fc06dc3246992b
sartajroshan <sarthajroshan@gmail.com>
2021-03-21 11:53:42 +0530

    sql added

--- project_billing.sql
+++ project_billing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 14, 2021 at 06:53 AM
+-- Generation Time: Mar 21, 2021 at 07:22 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
@@ -58,14 +58,16 @@ CREATE TABLE `bills` (
   `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `order_receiver_name` varchar(250) NOT NULL,
   `order_receiver_address` text NOT NULL,
-  `order_total_before_tax` decimal(10,2) NOT NULL,
-  `order_total_tax` decimal(10,2) NOT NULL,
-  `order_tax_per` varchar(250) NOT NULL,
-  `order_total_after_tax` double(10,2) NOT NULL,
-  `order_amount_paid` decimal(10,2) NOT NULL,
-  `order_total_amount_due` decimal(10,2) NOT NULL,
-  `note` text NOT NULL
+  `order_total_amount` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bills`
+--
+
+INSERT INTO `bills` (`order_id`, `user_id`, `order_date`, `order_receiver_name`, `order_receiver_address`, `order_total_amount`) VALUES
+(8, 1, '2021-03-21 06:15:28', 'Sartaj', 'ajjk', '120.00'),
+(9, 2, '2021-03-21 06:18:34', 'Vyshak', 'ajk', '100.00');
 
 -- --------------------------------------------------------
 
@@ -77,11 +79,17 @@ CREATE TABLE `bill_items` (
   `order_item_id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `item_code` varchar(250) NOT NULL,
-  `item_name` varchar(250) NOT NULL,
-  `order_item_quantity` decimal(10,2) NOT NULL,
-  `order_item_price` decimal(10,2) NOT NULL,
-  `order_item_final_amount` decimal(10,2) NOT NULL
+  `order_item_quantity` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bill_items`
+--
+
+INSERT INTO `bill_items` (`order_item_id`, `order_id`, `item_code`, `order_item_quantity`) VALUES
+(12, 8, '5', '1.00'),
+(13, 8, '6', '2.00'),
+(14, 9, '5', '1.00');
 
 -- --------------------------------------------------------
 
@@ -92,17 +100,16 @@ CREATE TABLE `bill_items` (
 CREATE TABLE `stock_details` (
   `id` int(11) NOT NULL,
   `item_name` varchar(255) NOT NULL,
-  `price` double NOT NULL,
-  `tax` double NOT NULL,
-  `stock` int(11) NOT NULL
+  `price` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `stock_details`
 --
 
-INSERT INTO `stock_details` (`id`, `item_name`, `price`, `tax`, `stock`) VALUES
-(1, 'item 1', 100, 2, 10);
+INSERT INTO `stock_details` (`id`, `item_name`, `price`) VALUES
+(5, 'Item 1', 100),
+(6, 'item 2', 10);
 
 --
 -- Indexes for dumped tables
@@ -146,19 +153,19 @@ ALTER TABLE `billing_users`
 -- AUTO_INCREMENT for table `bills`
 --
 ALTER TABLE `bills`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `bill_items`
 --
 ALTER TABLE `bill_items`
-  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `stock_details`
 --
 ALTER TABLE `stock_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
